52ac40f2fa35fab64db704ad4fe5a1881bd242db
ramprasad3c9 <87995887+ramprasad3c9@users.noreply.github.com>
2021-10-28 17:13:59 +0530

    Add files via upload

---
+++ bigger number.py
@@ -0,0 +1,8 @@
+a=int(input("enter a first number:"))
+b=int(input("enter a second number:"))
+if a>b:
+    print(a,"is bigger number than",b)
+elif a<b:
+    print(b,"is bigger number than",a)
+else:
+    print(a,"and",b,"are equal numbers")
---
+++ conditional statement on you can vote.py
@@ -0,0 +1,7 @@
+age=int(input("enter your age:"))
+if age>18:
+    print("you can vote")
+elif age<18:
+    print("you are not eligible for voting")
+else:
+    print("you can apply for voting")
