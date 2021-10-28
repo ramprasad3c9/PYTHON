b27dcd3570e9a4014b2a11670b7fe71ccc1bf361
ramprasad3c9 <87995887+ramprasad3c9@users.noreply.github.com>
2021-10-28 17:31:29 +0530

    Add files via upload

---
+++ digit numbers.py
@@ -0,0 +1,21 @@
+a=int(input("enter a digit:"))
+if a==1:
+    print("the digit is ONE")
+elif a==2:
+    print("the digit is TWO")
+elif a==3:
+    print("the digit is THREE")
+elif a==4:
+    print("the digit is FOUR")
+elif a==5:
+    print("the digit is FIVE")
+elif a==6:
+    print("the digit is SIX")
+elif a==7:
+    print("the digit is SEVEN")
+elif a==8:
+    print("the digit is EIGHT")
+elif a==9:
+    print("the digit is NINE")
+else:
+    print("enter digit not a number")
